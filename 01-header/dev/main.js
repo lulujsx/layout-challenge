f8bc36a95d4957adb11e6361b4daf85329408dad
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-12-05 20:19:28 -0300

    fixes in header

--- 01-header/dev/main.js
+++ 01-header/dev/main.js
@@ -9,3 +9,18 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     mobile_nav.classList.remove("visible");
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navCategory = document.querySelector('.nav-category-first');
+    var navMegamenu = document.querySelector('.nav-megamenu');
+
+    // Maneja el evento de hover
+    navCategory.addEventListener('mouseenter', function () {
+        navMegamenu.style.display = 'block';
+    });
+
+    // Maneja el evento cuando se sale del hover
+    navMegamenu.addEventListener('mouseleave', function () {
+        navMegamenu.style.display = 'none';
+    });
+});
