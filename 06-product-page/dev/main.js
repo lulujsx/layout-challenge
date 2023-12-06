7f99c41769ce167e6cf9c3cb84e38d4158dff44a
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-12-06 10:48:33 -0300

    fixes in 06-product-page

--- 06-product-page/dev/main.js
+++ 06-product-page/dev/main.js
@@ -10,23 +10,39 @@ close.addEventListener("click", () => {
     mobile_nav.classList.remove("visible");
 })
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const categories = document.querySelectorAll('.category');
 
-//     categories.forEach(category => {
-//         const title = category.querySelector('.category-title');
-//         const items = category.querySelector('.category-items');
-//         const icon = category.querySelector('.icon');
+document.addEventListener('DOMContentLoaded', function () {
 
-//         title.addEventListener('click', () => {
-            
-//             items.classList.toggle('active');
-//             // Cambiar la imagen cuando se activa/desactiva el toggle
-//             if (items.classList.contains('active')) {
-//                 icon.src = "../assets/lop-academy-icon-minus.svg"; // Ruta de la nueva imagen
-//             } else {
-//                 icon.src = "../assets/lop-academy-icon-plus.svg"; // Ruta de la imagen original
-//             }
-//         });
-//     });
-// });
+    // Menu functionality
+    const navCategory = document.querySelector('.nav-category-first');
+    const navMegamenu = document.querySelector('.nav-megamenu');
+
+    // Maneja el evento de hover
+    navCategory.addEventListener('mouseenter', function () {
+        navMegamenu.style.display = 'block';
+    });
+
+    // Maneja el evento cuando se sale del hover
+    navMegamenu.addEventListener('mouseleave', function () {
+        navMegamenu.style.display = 'none';
+    });
+
+    // Footer functionality
+    const categories = document.querySelectorAll('.category');
+
+    categories.forEach(category => {
+        const title = category.querySelector('.category-title');
+        const items = category.querySelector('.category-items');
+        const icon = category.querySelector('.icon');
+
+        title.addEventListener('click', () => {
+            items.classList.toggle('active');
+
+            if (items.classList.contains('active')) {
+                icon.src = "../assets/svg/lop-academy-icon-minus-white.svg"; // Ruta de la nueva imagen
+            } else {
+                icon.src = "../assets/svg/lop-academy-icon-plus-white.svg"; // Ruta de la imagen original
+            }
+        });
+    });
+});
